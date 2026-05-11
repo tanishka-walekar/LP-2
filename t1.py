da4ef96be749bd611124f4ff16fc7497ecc69bcf
Tanishka Walekar <tanishkawalekar7@gmail.com>
2026-05-11 23:22:53 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,1 +1,26 @@
-print("hello sangamner")
+print("Simple Calculator")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    print("Result =", a + b)
+
+elif choice == 2:
+    print("Result =", a - b)
+
+elif choice == 3:
+    print("Result =", a * b)
+
+elif choice == 4:
+    print("Result =", a / b)
+
+else:
+    print("Invalid Choice")
